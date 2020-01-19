932d602e7d84e382aaa6df96e54970649bd1bf96
Akshita Jain <43750664+Akshita207@users.noreply.github.com>
2020-01-19 19:52:54 +0530

    Update settings.py

--- y_search/settings.py
+++ y_search/settings.py
@@ -122,9 +122,8 @@ PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
 STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'static')
 
 STATIC_URL = '/static/'
-
- STATICFILES_DIRS = (
+STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
- )
+)
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
